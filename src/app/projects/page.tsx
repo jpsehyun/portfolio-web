fc9623a9cccd6f7c0a04951b9202960b8d35ed52
jpsehyun <jpsehyun@gmail.com>
2025-10-02 09:37:20 +0800

    external url updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,7 +13,7 @@ const projects = [
     title: "Trivia Game Show",
     desc: "Allows the users to play a trivia game. Powered by Open Trivia DB API.",
     tags: ["React", "TypeScript", "Tailwind"],
-    link: "trivia-gameshow.vercel.app",
+    link: "https://trivia-gameshow.vercel.app",
     img: "/images/trivia-thumb.png",
   },
   {
